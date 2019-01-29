360b22d2b7168865b50c7b1463b8a5ee512de49e
Harry <harry@coolnology.com>
2019-01-29 15:26:11 +1100

    Update index.js
    Bug fixes.

--- src/agenda/reservation-list/index.js
+++ src/agenda/reservation-list/index.js
@@ -189,10 +189,7 @@ class ReactComp extends Component {
   }
 
   render() {
-    if (
-      !this.props.reservations ||
-      !this.props.reservations[this.props.selectedDay.toString('yyyy-MM-dd')]
-    ) {
+    if (!this.props.reservations) {
       if (this.props.renderEmptyData) {
         return this.props.renderEmptyData()
       }
