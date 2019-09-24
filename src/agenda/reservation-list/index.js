9012f5620f816c38b22c43280289257611806b70
flyersea <36633594+flyersea@users.noreply.github.com>
2019-09-24 16:08:59 +1000

    Update index.js
    Make bigger day range.

--- src/agenda/reservation-list/index.js
+++ src/agenda/reservation-list/index.js
@@ -179,7 +179,7 @@ class ReactComp extends Component {
     }
     const scrollPosition = reservations.length
     const iterator = props.selectedDay.clone()
-    for (let i = 0; i < 31; i++) {
+    for (let i = 0; i < 93; i++) {
       const res = this.getReservationsForDay(iterator, props)
       if (res) {
         reservations = reservations.concat(res)
